8e211d6a790f044aaae6abdb04db9b21585fe553
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-08-03 11:37:12 -0400

    Arrange the header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,95 @@
 /* The top part of the page  */
 import "./Header.css";
 
+/* A function for each navigation item in the header.
+ * When clicked it highlights itself and switches what is
+ * presented in the contect section.
+ * Props contains the name if the item currently selected
+ * and the function to call when the item is clicked.
+ */
+function AboutMe(props) {
+  let selectedQ = "";
+  if (props.section_name === "About_me") {
+    selectedQ = "selected";
+  } else {
+    selectedQ = "not_selected";
+  }
+  return (
+    <span className={selectedQ} onClick={props.onClick}>
+      About Me
+    </span>
+  );
+}
+
+function Portfolio(props) {
+  let selectedQ = "";
+  if (props.section_name === "Portfolio") {
+    selectedQ = "selected";
+  } else {
+    selectedQ = "not_selected";
+  }
+  return (
+    <span className={selectedQ} onClick={props.onClick}>
+      Portfolio
+    </span>
+  );
+}
+
+function Contact(props) {
+  let selectedQ = "";
+  if (props.section_name === "Contact") {
+    selectedQ = "selected";
+  } else {
+    selectedQ = "not_selected";
+  }
+  return (
+    <span className={selectedQ} onClick={props.onClick}>
+      Contact
+    </span>
+  );
+}
+
+function Résumé(props) {
+  let selectedQ = "";
+  if (props.section_name === "Résumé") {
+    selectedQ = "selected";
+  } else {
+    selectedQ = "not_selected";
+  }
+  return (
+    <span className={selectedQ} onClick={props.onClick}>
+      Résumé
+    </span>
+  );
+}
+
+/* Display my name.  */
+function Name(props) {
+  return (<span className="my_name">John Sauter</span>);
+}
+
 export default function Header(props) {
-  return <div>Header</div>;
+  /* Functions to respond to clicks on header items  */
+  function click_about_me(e) {
+    props.setSection_name("About_me");
+  }
+  function click_portfolio(e) {
+    props.setSection_name("Portfolio");
+  }
+  function click_contact(e) {
+    props.setSection_name("Contact");
+  }
+  function click_résumé(e) {
+    props.setSection_name("Résumé");
+  }
+
+  return (
+    <nav className="header_container">
+      <Name />
+      <AboutMe section_name={props.section_name} onClick={click_about_me} />
+      <Portfolio section_name={props.section_name} onClick={click_portfolio} />
+      <Contact section_name={props.section_name} onClick={click_contact} />
+      <Résumé section_name={props.section_name} onClick={click_résumé} />
+    </nav>
+  );
 }
