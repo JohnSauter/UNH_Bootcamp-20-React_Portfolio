3f1da3b58ea9ca93ee545ae994a976d2e77f84fb
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-08-03 11:48:40 -0400

    Fill out the footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,5 +2,19 @@
 import "./Footer.css";
 
 export default function Footer(props) {
-  return <div>Footer</div>;
+  return (
+    <nav className="footer_container">
+      <span>
+        <a href="https:github.com/JohnSauter/">GitHub profile</a>
+      </span>
+      <span>
+        <a href="https://www.linkedin.com/in/john-sauter-9580b2232/">
+          Linkedin profile
+        </a>
+      </span>
+      <span>
+        <a href="https://www.systemeyescomputerstore.com/">personal web site</a>
+      </span>
+    </nav>
+  );
 }
