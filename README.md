e5d93512224839b0dee54ce50d5a9dc25c88b6df
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-08-07 11:01:41 -0400

    Add a good README file

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # UNH_Bootcamp-20-React_Portfolio
-portfolio for those looking for a react-capable programmer
+[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
+
+## Description
+
+Show my work to persons interested in a React-capable programmer.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+
+git clone https://github.com/JohnSauter/UNH_Bootcamp-20-React_Portfolio.git
+
+cd https://github.com/JohnSauter/UNH_Bootcamp-20-React_Portfolio
+
+npm install
+
+npm run start
+
+## Usage
+
+You will see tabs for About Me, Portfolio, Contact and Résumé.
+
+## Credits
+
+React and  GitHub
+
+## License
+
+GNU AGPLv3
+
+## Features
+
+There is good feedback when entering data in the Contact form.
+
+## Questions
+
+If you have any questions, contact me.
+My e-mail address is John_Sauter@systemeyescomputerstore.com.
+My github user name is JohnSauter.  See my profile at
+https://github.com/JohnSauter.
+
--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -9,21 +9,21 @@ function Portfolio(props) {
     <div className="portfolio_container">
       <Project
         title="Find Dry Chargers"
-        image_href="./assets/images/find_dry_chargers.png"
+        image_src="./assets/images/find_dry_chargers.png"
         URL1="https://github.com/Jay3172/Find_Dry_Chargers"
         URL2="https://jay3172.github.io/Find_Dry_Chargers/"
       />
 
       <Project
         title="Tech Blog"
-        image_href="./assets/images/The_Tech_Blog.png"
+        image_src="./assets/images/The_Tech_Blog.png"
         URL1="https://github.com/JohnSauter/UNH_Bootcamp-14-Tech_Blog"
         URL2="https://damp-sea-07082.herokuapp.com/"
       />
 
       <Project
         title="Employee Tracker"
-        image_href="./assets/images/Employee_tracker.jpg"
+        image_src="./assets/images/Employee_tracker.jpg"
         URL1="https://github.com/JohnSauter/UNH_Bootcamp-12-Employee_Tracker"
         URL2="https://github.com/JohnSauter/UNH_Bootcamp-12-Employee_Tracker/blob/main/videos/Walkthrough.mp4"
         URL2_label="Walkthrough URL"
@@ -31,14 +31,14 @@ function Portfolio(props) {
 
       <Project
         title="Weather Dashboard"
-        image_href="./assets/images/Weather_Dashboard.png"
+        image_src="./assets/images/Weather_Dashboard.png"
         URL1="https://github.com/JohnSauter/UNH_bootcamp-06-Weather_Dashboard"
         URL2="https://johnsauter.github.io/UNH_bootcamp-06-Weather_Dashboard/"
       />
 
       <Project
         title="Plant Selection Planner"
-        image_href="./assets/images/plant_selection_planner.png"
+        image_src="./assets/images/plant_selection_planner.png"
         URL1="https://github.com/JohnSauter/plant_selection_planner"
         URL2="https://beautiful-yosemite-29690.herokuapp.com/"
       />
