76df04c6cf71dc92ba11a694b66770bb74d08a87
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-08-07 12:24:27 -0400

    Fix github profile

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,15 +5,31 @@ export default function Footer(props) {
   return (
     <nav className="footer_container">
       <span>
-        <a href="https:github.com/JohnSauter/" rel="noreferrer" target="_blank">GitHub profile</a>
+        <a
+          href="https://github.com/JohnSauter/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          GitHub profile
+        </a>
       </span>
       <span>
-        <a href="https://www.linkedin.com/in/john-sauter-9580b2232/" rel="noreferrer" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/john-sauter-9580b2232/"
+          rel="noreferrer"
+          target="_blank"
+        >
           Linkedin profile
         </a>
       </span>
       <span>
-        <a href="https://www.systemeyescomputerstore.com/" rel="noreferrer" target="_blank">personal web site</a>
+        <a
+          href="https://www.systemeyescomputerstore.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          personal web site
+        </a>
       </span>
     </nav>
   );
