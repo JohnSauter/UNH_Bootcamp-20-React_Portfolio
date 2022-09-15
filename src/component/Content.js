8c7d7d0de4254909274bfb39e01c38d59e4c4288
John <John_Sauter@systemeyescomputerstore.com>
2022-09-15 11:47:15 -0400

    rewrite the contact page

--- src/component/Content.js
+++ src/component/Content.js
@@ -1,7 +1,9 @@
 /* The content part of the page  */
 import "./Content.css";
 
+/* The contact form is not used.  
 import ContactForm from "./ContactForm.js";
+ */
 import Portfolio from "./Portfolio.js";
 
 export default function Content(props) {
@@ -38,18 +40,18 @@ export default function Content(props) {
   if (props.section_name === "Technical_skills") {
     return (
       <div className="content_container">
-        
+
         <p className="text_2">
-          I am skilled in using HTML, CSS, 
-          JavaScript, Node.js and 
+          I am skilled in using HTML, CSS,
+          JavaScript, Node.js and
           their many companion packages
           to create both the front-end
           and back-end parts of a web application.
         </p>
         <p className="text_2">
-          In addition to JavaScript I can code in C, Python, 
+          In addition to JavaScript I can code in C, Python,
           Bliss, Basic, Ada, Fortran, SQL and assembly
-          language for a variety of computers.  
+          language for a variety of computers.
           I have written batch, interactive
           and real-time software.
         </p>
@@ -61,18 +63,19 @@ export default function Content(props) {
     return (
       <div className="content_container">
         <p className="text_2">
-          <a href="./assets/documents/Résumé.pdf" target="_blank">
+          <a href="./assets/documents/Résumé.pdf"
+            target="_blank" rel="noreferrer">
             You can download my résumé by clicking here.
           </a>
         </p>
         <p className="text_2">
           Beyond what is detailed on my résumé, I have worked
-          as a system programmer for the 
-          Stanford Artificial Intelligence project 
+          as a system programmer for the
+          Stanford Artificial Intelligence project
           and for Sanders Associates, a military
-          contractor. I have also worked as a 
+          contractor. I have also worked as a
           Software Engineer for Digital Equipment
-          Corporation, creating and maintaining 
+          Corporation, creating and maintaining
           products for a world-class
           computer manufacturer.
         </p>
@@ -81,10 +84,74 @@ export default function Content(props) {
   }
 
   if (props.section_name === "Contact") {
+    /* The contact form is fake, as required by the assignment.
+     * Replace it by some text.
     return (
       <div className="contact_form_container">
         <ContactForm />
       </div>
+    );
+    */
+    return (
+      <div className="contact_container">
+        <p className="text_2">
+          I prefer to be contacted by email.
+          My email address is: {" "}
+          <a href="mailto:John_Sauter@systemeyescomputerstore.com">
+            John_Sauter@systemeyescomputerstore.com</a>
+        </p>
+        <p className="text_2">
+          Alternatively, you can write to me at this address:
+        </p>
+        <p className="text_pre">
+          John Sauter<br />
+          System Eyes Computer Store<br />
+          20A Northwest Bvld.  Ste 345<br />
+          Nashua, NH  03063-4066<br />
+          USA</p>
+
+        <p className="text_2">
+          If you prefer voice communication
+          you can call me at (603) 424-1188.
+          That number is guarded by a robocall prevention device
+          which requires you to pass an intelligence test
+          before ringing through to me.
+        </p>
+        <p className="text_2">
+          I do not communicate using text messages or FaceBook,
+          but if you would like to use Google Meet or Zoom
+          contact me by email or telephone can we can make
+          arrangements.
+        </p>
+        <p className="text_2">
+          If you are concerned that email may not be secure,
+          we can use PGP to sign and encrypt our email
+          conversations.  I routinely sign my email messages.
+          You can verify the signature using my public key
+          which you can retrieve using this command:
+        </p>
+        <p className="text_pre">
+          gpg --locate-external-keys John_Sauter@systemeyescomputerstore.com
+        </p>
+        <p className="text_2">
+          <a href="./assets/documents/John_Sauter_public_key.asc"
+            target="_blank">
+            You can also download it by clicking here.
+          </a>
+        </p>
+        <p className="text_2">
+          Once you have my PGP public key
+          you can also sign your messages to me.
+          When you do, please let me know how to find your public key.
+          You might, for example, attach it to your e-mail message.
+          When we both have each other's public keys we can not only
+          sign our messages to each other but also encrypt them.
+          See <a href="https://www.openpgp.org/"
+            target="_blank" rel="noreferrer">
+            https://www.openpgp.org</a> for more information.
+        </p>
+
+      </div >
     );
   }
 
